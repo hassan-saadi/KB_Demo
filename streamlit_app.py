c469bc85165db6bc40e95a16fde03e385164fa3b
andychak <100386469+andychak@users.noreply.github.com>
2023-02-09 10:28:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,18 +13,7 @@ ctx = snowflake.connect(user=os.environ['snowflake_user'],password=os.environ['s
 cs = ctx.cursor()
 try:
     cs.execute("""        
-            select node_distance, starting_node, ending_node,array_agg(distinct concat(nvl(ievent,''),' : ', nvl(ievents2.label,''), ' : ',nvl(topics.name,''))) as category_event_and_topic, sum(intensity) as intensity
-            , round(nvl(avg(codocs.sentiment),0),3) as endnode_sentiment, round(nvl(avg(codocs.financialsentiment),0),3) as endnode_financialsentiment
-            , round(nvl(avg(codocs2.sentiment),0),3) as startnode_sentiment, round(nvl(avg(codocs2.financialsentiment),0),3) as startnode_financialsentiment
-            from "SCRATCH_FORGEAI"."ANDYC"."KEVINBACON" KB
-            left join "PROD_V06XX"."COMPANY_DATASET_V0650"."COMPANYSENTIMENTS" codocs on codocs.uripath = ending_node and codocs.confidence >=0.6 
-            left join "PROD_V06XX"."COMPANY_DATASET_V0650"."COMPANYSENTIMENTS" codocs2 on codocs2.uripath = starting_node and codocs.docid = codocs2.docid and codocs2.confidence >=0.6
-            left join "PROD_V06XX"."ARTICLES_V0670"."DOCUMENTTOPICS" topics on topics.docid = codocs.docid and topics.score >= .7
-            left join "DEV_V06XX"."ARTICLES_V0670"."INTRINSICEVENTS" ievents2 on ievents2.docid = codocs.docid and ievents2.confidence >=.7
-            where graph = 'goldman_sachs_group_inc'
-            and KB.ievent is not null
-            and KB.ievent not in ('Communication', '')
-            group by 1 , 2,3
+            select * from "SCRATCH_FORGEAI"."ANDYC"."KBDemo"
            ;""")
     rows_big = cs.fetchall()
 except:
