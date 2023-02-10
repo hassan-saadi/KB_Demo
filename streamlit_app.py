da0358ea7d2fb0a0ae1f948c23c9a0d84fed2d3b
andychak <100386469+andychak@users.noreply.github.com>
2023-02-10 08:11:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import requests
 
 st.sidebar.image('https://fiscalnote-marketing.s3.amazonaws.com/logo-FN-white-red.png')
 st.sidebar.title ('Welcome to the RiskConnector Sample Preview')
-st.sidebar.text ('Limited connections per node are shown')
+st.sidebar.text ('Limited connections shown')
 data = pd.read_csv('https://raw.githubusercontent.com/andychak/KB_Demo/master/result.csv')
 graphname = st.sidebar.selectbox("Please select a company as a starting node:", data['GRAPH'].unique())
 
