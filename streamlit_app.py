d23e958eb38c583d8d05b80380ba6ccfe4780b2c
andychak <100386469+andychak@users.noreply.github.com>
2023-02-09 21:00:58 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ for index, row in df_graph.iterrows():
 # Display the network in Streamlit
 #st.write(net.show("my_network.html"), unsafe_allow_html=True)
 # Save the network chart as an HTML file
-net.save("network.html")
+net.write_html("network.html")
 
 # Display the HTML content in Streamlit
 st.html(open("network.html").read(), unsafe_allow_html=True)
