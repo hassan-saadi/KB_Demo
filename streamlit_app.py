a7f45e3663c0934af23cc8ed351f8545c0b05b75
andychak <100386469+andychak@users.noreply.github.com>
2023-02-09 16:20:55 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 st.title ('Welcome to the FiscalNote _DeepRisk360_ Demo')
-data = pd.read_csv('https://raw.githubusercontent.com/andychak/KB_Demo/master/data/result.csv?token=GHSAT0AAAAAAB4J2SZYQQUUCBVEGWDZWT5IY7FRPNA')
+data = pd.read_csv('https://raw.githubusercontent.com/andychak/KB_Demo/master/result.csv')
 
 data
 
