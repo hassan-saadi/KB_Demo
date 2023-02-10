8b311f83548a61144c6ea372ae24dff694f35c83
andychak <100386469+andychak@users.noreply.github.com>
2023-02-09 20:08:15 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 st.title ('Welcome to the FiscalNote _DeepRisk360_ Demo')
 data = pd.read_csv('https://raw.githubusercontent.com/andychak/KB_Demo/master/result.csv')
+graphname = st.selectbox("Please select a company as a starting node:", data['GRAPH'].unique())
+graphname
 
-data
 
