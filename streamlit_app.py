c259ed442fd20c74f57cf633aee94e4dbffd91b7
andychak <100386469+andychak@users.noreply.github.com>
2023-02-09 20:21:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ from pyvis.network import Network
 import streamlit as st
 import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-image = Image.open('https://fiscalnote-marketing.s3.amazonaws.com/logo-FN-white-red.png')
-st.sidebar.image(image)
+st.sidebar.image('https://fiscalnote-marketing.s3.amazonaws.com/logo-FN-white-red.png')
 st.sidebar.title ('Welcome to RiskHorizon')
 data = pd.read_csv('https://raw.githubusercontent.com/andychak/KB_Demo/master/result.csv')
 graphname = st.sidebar.selectbox("Please select a company as a starting node:", data['GRAPH'].unique())
