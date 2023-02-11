85c3d3baf8aa9a1ed075f972191b8dc492b8ece9
andychak <100386469+andychak@users.noreply.github.com>
2023-02-11 10:53:16 -0800

    Update 2_World_Map_Recent_Events.py

--- pages/2_World_Map_Recent_Events.py
+++ pages/2_World_Map_Recent_Events.py
@@ -62,7 +62,7 @@ graph_df = mapdf[mapdf['GRAPH']==graphname]
 m = folium.Map(control_scale=True, attr="CQ RiskConnector")
 for i, row in graph_df.iterrows():
     folium.Marker([row['LATITUDE'], row['LONGITUDE']], popup="https://fiscalnote.com/", tooltip=row['ENDING_NODE'], 
-                   color = row['SENTIMENT_COLOR']).add_to(m)
+                   icon=folium.Icon(icon='marker', color=row['SENTIMENT_COLOR'])).add_to(m)
 
 # call to render Folium map in Streamlit
 st_data = st_folium(m, width = 1200)
