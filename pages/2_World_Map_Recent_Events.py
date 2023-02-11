b3ef001c2ad6ea37432ea435865e4e057efee568
andychak <100386469+andychak@users.noreply.github.com>
2023-02-11 10:36:12 -0800

    Update 2_World_Map_Recent_Events.py

--- pages/2_World_Map_Recent_Events.py
+++ pages/2_World_Map_Recent_Events.py
@@ -10,7 +10,6 @@ import requests
 import folium
 from streamlit_folium import st_folium
 st.set_page_config(page_title = 'CQ RiskConnector', layout="wide")
-st.sidebar.image('https://images.app.goo.gl/AUwzfJvUjPXDVu1c6')
 st.sidebar.image('https://upload.wikimedia.org/wikipedia/commons/0/05/CQ_Logo.jpeg', width = 40)
 st.sidebar.caption("Improving Your :blue[C]hange :blue[Q]uotient")
 #st.sidebar.image('https://fiscalnote-marketing.s3.amazonaws.com/logo-FN-white-red.png')
@@ -34,34 +33,38 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 query = """
-            select distinct any_value(graph), any_value(to_date(nvl(docdatetime, processdatetime))) as date, ENDING_NODE, any_value(url), 
+select distinct any_value(graph), any_value(node_distance), any_value(to_date(nvl(docs.docdatetime, docs.processdatetime))) as date, ENDING_NODE, any_value(url), 
+            any_value(case when sentiment is NULL then 'gray' when sentiment <= -.02 then 'red' when sentiment >= .3 then 'green' ELSE 'gray' END) as sentimentcolor,
             any_value(latitude) as latitude, any_value(longitude) as longitude
-            from "SCRATCH_FORGEAI"."ANDYC"."KBWEB" kb
-            join "PROD_V06XX"."FORGEAI_SURGES"."DOCUMENTS" docs on docs.uripath = kb.ENDING_NODE and docs.saliency_V1 >= .25
-            join "PROD_V06XX"."ARTICLES_V0670"."INTRINSICEVENTS" ievents on ievents.docid = docs.docid and ievents.confidence >= 0.35
-            join "PROD_V06XX"."ARTICLES_V0670"."ENTITYASSERTIONS" serts on serts.docid = docs.docid and serts.confidence >= 0.7
-            join "PROD_V06XX"."KG_V0660"."LOCATIONS" locs on locs.type = serts.assertedclass where starting_node != ending_node
-            and ending_node not in ('pepsico_inc', 'coca-cola_company', 'goldman_sachs_group_inc', 'jp_morgan_chase_and_co'
-                        , 'alphabet_inc', 'microsoft_corp', 'google')
-            and category in ('NaturalDisaster', 'Legal', 'Political', 'Government', 'SupplyChain', 'Contract', 'Basel', 'Financing', 'Corporate')
-            and url not like('https://forgeai.net/naviga%')
-            and label not in ('earnings call', 'Earnings Call')
-            and latitude is not NULL
-            and docdatetime >= DATEADD(day, -30, CURRENT_DATE())
-            group by 3 limit 600;"""
+  from "SCRATCH_FORGEAI"."ANDYC"."KBWEB" kb
+  join "PROD_V06XX"."FORGEAI_SURGES"."DOCUMENTS" docs on docs.uripath = kb.ENDING_NODE and docs.saliency_V1 >=.25
+  
+  join "PROD_V06XX"."ARTICLES_V0670"."INTRINSICEVENTS" ievents on ievents.docid = docs.docid //and ievents.confidence >= 0.15
+  join "PROD_V06XX"."ARTICLES_V0670"."ENTITYASSERTIONS" serts on serts.docid = docs.docid and serts.confidence >= 0.6
+  join  "PROD_V06XX"."KG_V0660"."LOCATIONS" locs on locs.type = serts.assertedclass
+  where starting_node != ending_node
+  and ending_node not in ('pepsico_inc','coca-cola_company','goldman_sachs_group_inc','jp_morgan_chase_and_co'
+        ,'alphabet_inc' ,'microsoft_corp', 'google')
+  and category in ('NaturalDisaster', 'Legal', 'Political', 'Government', 'SupplyChain', 'Contract', 'Basel', 'Financing', 'Corporate')
+  and url not like ('https://forgeai.net/naviga%')
+  and label not in ('earnings call', 'Earnings Call')
+  and latitude is not NULL
+  and docs.docdatetime >= DATEADD(day, -60, CURRENT_DATE())
+  group by 4
+            ;"""
 rows = run_query(query)
-mapdf =  pd.DataFrame(rows, columns = ['GRAPH', 'DATE', 'ENDING_NODE', 'URL', 'LATITUDE', 'LONGITUDE'])
+mapdf =  pd.DataFrame(rows, columns = ['GRAPH','NODE_DISTANCE', 'DATE', 'ENDING_NODE','SENTIMENT_COLOR', 'URL', 'LATITUDE', 'LONGITUDE'])
 graphname = st.sidebar.selectbox("Please select a company as a starting node:", data['GRAPH'].unique())
 graph_df = mapdf[mapdf['GRAPH']==graphname]
 
 
-m = folium.Map()
-folium.Marker(
-    [39.949610, -5.150282], popup="https://fiscalnote.com/", tooltip="Liberty Bell"
-).add_to(m)
+m = folium.Map(control_scale=True, attr="CQ RiskConnector")
+for i, row in graph_df.iterrows():
+    folium.Marker([row['LATITUDE'], row['LONGITUDE'], popup="https://fiscalnote.com/", tooltip=row['ENDING_NODE'], 
+                   color = row['SENTIMENT_COLOR']).add_to(m)
 
 # call to render Folium map in Streamlit
-st_data = st_folium(m, width=1000)
+st_data = st_folium(l, width=1000)
 
 # Footer
 st.sidebar.markdown(
